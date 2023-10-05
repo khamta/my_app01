467fefafc6a17cecd55845b45a02af7d9dfa0a77
khamta <khamta87@gmail.com>
2023-10-05 11:19:17 +0700

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+       <h1> Hello vue</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App1App',
+    setup(){
+
+    },
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+    computed:{
+
+    },
+    watch:{
+
+    },
+    created(){
+
+    },
+    beforeRouteEnter(to,from,next){
+
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
